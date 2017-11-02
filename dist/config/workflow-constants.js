114a1bcd7e16026596d5cba85d2bbfcbe7ba99a6
Philip Moss <philmoss543@gmail.com>
2017-11-02 14:01:55 -0700

    remove dist

--- dist/config/workflow-constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ENCODE_HLS = 'encodeHls';
-exports.ENCODE_VIDEO = 'encodeVideo';
-exports.S3UPLOAD = 's3Upload';
-exports.DEFAULT = 'default';
-//# sourceMappingURL=workflow-constants.js.map
